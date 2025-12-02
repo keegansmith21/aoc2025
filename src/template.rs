94593c42e92b51bfda314edb139cbaa2e7295122
keeagnsmith21 <keegan.r.s21@gmail.com>
2025-12-03 07:41:50 +0800

    template update

--- src/template.rs
+++ src/template.rs
@@ -1,15 +1,16 @@
 use crate::utils;
 
 pub fn main(example: bool) {
-    let input = utils::read_input(1, example);
+    let day: u8 = n;
+    let input = utils::read_input(day, example);
 
-    println!("Day N:");
+    println!("Day {}:", day);
 
     let p1 = solve_part1(&input);
     let p2 = solve_part2(&input);
 
-    println!("Part 1: {}", p1);
-    println!("Part 2: {}", p2);
+    println!("Day {} - Part 1: {}", day, p1);
+    println!("Day {} - Part 2: {}", day, p2);
 }
 
 fn solve_part1(_input: &str) -> i64 {
